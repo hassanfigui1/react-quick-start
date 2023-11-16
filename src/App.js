5c6e6eca9fa77a42627ca334f12ed04ef1129885
hassan figuigui <hassan.figuigui7@aiesec.net>
2023-11-16 12:49:16 +0100

    implementing add component ui using MaterialUI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+// import '@fontsource/roboto/300.css';
+// import '@fontsource/roboto/400.css';
+// import '@fontsource/roboto/500.css';
+// import '@fontsource/roboto/700.css';
 import React from 'react';
 import './App.css';
+import CustomizedDialogs from './dialogs/add-dialog.component';
 
 class App extends React.Component {
   render() {
-    return <h1>FDFDFS</h1>;
+    return <CustomizedDialogs name={'hassan figuigui'}></CustomizedDialogs>;
   }
 }
 // function App() {
