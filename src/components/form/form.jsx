2930f9183a5ca4e313d0ee8d8ed1a0a385e59635
hassan figuigui <hassan.figuigui7@aiesec.net>
2023-11-21 15:00:48 +0100

    last updates

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -1,13 +1,79 @@
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 import React, { Component } from 'react';
 import './form.css';
 
 export class Form extends Component {
-  render(){
-    return <div>
-        <h1>Can I get your what's app number ? </h1>
-        <h2>I'm Interested to know u </h2>
-        <h2>What's you name ? </h2>
-    </div>
+  constructor(props) {
+    super(props);
+    this.result = '';
+    this.state = {
+      username: '',
+    };
+  }
+  handlerUsernameChange = (event) => {
+    this.setState({
+      username: event.target.value,
+    });
+  };
+  commentHandler = (event) => {
+    this.setState({
+      comments: event.target.value,
+    });
+  };
+  skillHandler = (event) => {
+    this.setState({
+      skill: event.target.value,
+    });
+  };
+  handleSubmit = (event) => {
+    alert(`${this.state.username}`);
+  };
+  render() {
+    return (
+      <div>
+        <h1>Hello : {this.result}</h1>
+        <form className='flex' onSubmit={this.handleSubmit}>
+          <div className='flex-row'>
+            <label htmlFor='Username'>Username </label>
+            <TextField
+              id='outlined-basic'
+              value={this.state.username}
+              onChange={this.handlerUsernameChange}
+              label='Outlined'
+              required
+              variant='outlined'
+              helperText='Some important text'
+            />
+          </div>
+          <div className='flex-row'>
+            <label htmlFor=''>Comments</label>
+            <textarea
+              value={this.state.comments}
+              name=''
+              onChange={this.commentHandler}
+              cols='19'
+              rows='2'></textarea>
+          </div>
+          <div className='flex-row'>
+            <label
+              htmlFor=''
+              value={this.state.skill}
+              onChange={this.skillHandler}>
+              Topic
+            </label>
+            <select name='' id=''>
+              <option value='React'>React</option>
+              <option value='Angular'>Angular</option>
+              <option value='Vue Js'>Vue Js</option>
+            </select>
+          </div>
+          <div className='mt-24 flex-row flex-right'>
+            <Button variant='contained' onClick={()=>alert(this.state.username)}>Submit</Button>
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
